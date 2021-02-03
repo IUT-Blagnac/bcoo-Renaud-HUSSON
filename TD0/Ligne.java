c26fe715835aadca7a48a1b011f7200afa6f4cad
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-02-03 15:40:49 +0100

    fix 0.5.2: Ajout de la classe Ligne

---
+++ TD0/Ligne.java
@@ -0,0 +1,20 @@
+public class Ligne {
+  private Point pointDebut;
+  private Point pointFin;
+
+  public Point getPointDebut(){
+    return this.pointDebut;
+  }
+
+  public void setPointDebut(Point pointDebut){
+    this.pointDebut = pointDebut;
+  }
+
+  public Point getPointFin(){
+    return this.pointFin;
+  }
+
+  public void setPointFin(Point pointFin){
+    this.pointFin = pointFin;
+  }
+}
