86e579a22acc65431e361e63e70cb3bd5697b886
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-02-03 15:01:06 +0100

    fix #0.3: Ajout de la classe Point

---
+++ TD0/Point.java
@@ -0,0 +1,21 @@
+Class Point {
+  private int x;
+  private int y;
+
+  //GETTERS & SETTERS
+  public int getX(){
+    return this.x;
+  }
+
+  public void setX(x){
+    this.x = x;
+  }
+  
+  public int getY(){
+    return this.x;
+  }
+
+  public void setY(y){
+    this.y = y;
+  }
+}
